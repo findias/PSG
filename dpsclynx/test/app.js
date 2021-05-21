6ae63be0f52e496c28113dfcb97f6f6d4f4ff1bc
Konstantin Kovalev <findias@bk.ru>
2021-05-21 23:58:51 +0300

    first

---
+++ dpsclynx/test/app.js
@@ -0,0 +1,29 @@
+const options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+};
+const someError = () => {
+    console.error(error)
+}
+
+
+
+const getPosition = () => {
+    if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition,someError,options);
+    } else {
+        alert("Версия вашего браузера не позволяет определить ваше местоположение");
+    }
+}
+
+const showPosition = (position) => {
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
+    const timePosition = new Date(position.timestamp)
+    console.log('Широта: '+ latitude + ', Долгота: ' + longitude + ', Время сбора информации: ' + timePosition)
+    return latitude
+}
+
+getPosition()
+
